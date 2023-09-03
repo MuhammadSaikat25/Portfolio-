ff648b6dd563302f4107d8ac876acb87a799c2cc
MuhammadSaikat25 <smsaikat000@gmail.com>
2023-09-03 16:12:28 +0600

    added My Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import About from "./component/About";
 import Home from "./component/Home";
 import Nav from "./component/Nav";
+import MyProject from "./component/Project/MyProject";
 
 const App = () => {
   return (
     <div>
-       <Nav></Nav>
+      <Nav></Nav>
       <Home></Home>
       <About></About>
+      <MyProject></MyProject>
     </div>
   );
 };
